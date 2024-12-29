35ab5680f88db4fe4f503a58a66edae47a735334
Mark Ciril <markciril.jamon@gmail.com>
2024-12-29 15:37:08 +0800

    feat: add a filter function to get only all the csv files in the given directory

--- src/extractor.ts
+++ src/extractor.ts
@@ -1,11 +1,13 @@
 import fs from 'fs'
+import path from 'path'
 
 const logfilesPath = '../logfiles'
 
-const files = fs.readdirSync(logfilesPath);
+const files = fs.readdirSync(logfilesPath)
+const filteredFiles = files.filter((file) => path.extname(file) == '.csv')
 
-for (let index = 0; index < files.length; index++) {
-    const extracted = fs.readFileSync(`${logfilesPath}/${files[index]}`, {
+for (let index = 0; index < filteredFiles.length; index++) {
+    const extracted = fs.readFileSync(`${logfilesPath}/${filteredFiles[index]}`, {
         encoding: 'utf-8'
     }).split('\r\n').map((row: string): string[] => {
         return row.split(',')
