b4585ba2949dd34ee02d69bd6a4cdc40b948e1dd
Mark Ciril <markciril.jamon@gmail.com>
2024-12-29 18:07:19 +0800

    refactor: Introduce FileMetadata interface to enforce type safety for processing file informations

--- src/extractor.ts
+++ src/extractor.ts
@@ -1,13 +1,14 @@
 import fs from 'fs'
 import path from 'path'
+import type { FileMetadata } from './types/index'
 
 const logfilesPath = '../logfiles'
 
 const files = fs.readdirSync(logfilesPath)
 const filteredFiles = files.filter((file) => path.extname(file) == '.csv')
 
-const processFileInfo = (path: string, file: string) => {
-    const extracted = fs.readFileSync(`${path}/${file}`, {
+const processFileInfo = ({path, filename}: FileMetadata) => {
+    const extracted = fs.readFileSync(`${path}/${filename}`, {
         encoding: 'utf-8'
     }).split('\r\n').map((row: string): string[] => {
         return row.split(',')
@@ -15,17 +16,19 @@ const processFileInfo = (path: string, file: string) => {
     return extracted
 }
 
-const extractInformation = (path: string, files: string[] | string) => {
-    let extractedInfomations: any = []
-    if (Array.isArray(files)) {
-        for (let index = 0; index < files.length; index++) {
-            extractedInfomations.push = processFileInfo(path, files[index])
+const extractInformation = (path: string, filename: string | string[]) => {
+    let extractedInformations: any[] = []
+
+    // Check if filename is an array to handle multiple files
+    if (Array.isArray(filename)) { 
+        for (let index = 0; index < filename.length; index++) {
+            extractedInformations.push(processFileInfo({ path, filename: filename[index] }))
         }
     } else {
-        extractedInfomations = processFileInfo(path, files)
+        // Single file case
+        extractedInformations = [processFileInfo({ path, filename })] 
     }
-
-    return extractedInfomations
+    return extractedInformations
 }
 
 // single file simulation
