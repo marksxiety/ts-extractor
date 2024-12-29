14f15339fb58786aca03add62b0b1cd367de0724
Mark Ciril <markciril.jamon@gmail.com>
2024-12-29 17:25:46 +0800

    feat: make the functions adoptable if the passed parameter is single file or multiple file

--- src/extractor.ts
+++ src/extractor.ts
@@ -6,11 +6,30 @@ const logfilesPath = '../logfiles'
 const files = fs.readdirSync(logfilesPath)
 const filteredFiles = files.filter((file) => path.extname(file) == '.csv')
 
-for (let index = 0; index < filteredFiles.length; index++) {
-    const extracted = fs.readFileSync(`${logfilesPath}/${filteredFiles[index]}`, {
+const processFileInfo = (path: string, file: string) => {
+    const extracted = fs.readFileSync(`${path}/${file}`, {
         encoding: 'utf-8'
     }).split('\r\n').map((row: string): string[] => {
         return row.split(',')
     })
-    console.log(extracted)
+    return extracted
 }
+
+const extractInformation = (path: string, files: string[] | string) => {
+    let extractedInfomations: any = []
+    if (Array.isArray(files)) {
+        for (let index = 0; index < files.length; index++) {
+            extractedInfomations.push = processFileInfo(path, files[index])
+        }
+    } else {
+        extractedInfomations = processFileInfo(path, files)
+    }
+
+    return extractedInfomations
+}
+
+// single file simulation
+// const extractedInfo = extractInformation(logfilesPath, 'testlogfile1.csv')
+// multiple file simulation
+const extractedInfo = extractInformation(logfilesPath, filteredFiles)
+console.log(extractedInfo)
