651634809c5dac953aff9750609be94f2730faa1
Mark Ciril <markciril.jamon@gmail.com>
2024-12-29 14:36:09 +0800

    feat: parse CSV file into an array of rows and columns

--- src/extractor.ts
+++ src/extractor.ts
@@ -2,6 +2,8 @@ import fs from 'fs'
 
 const matches = fs.readFileSync('../file.csv', {
     encoding: 'utf-8'
+}).split('\r\n').map((row:string): string[] => {
+    return row.split(',')
 })
 
 console.log(matches)
